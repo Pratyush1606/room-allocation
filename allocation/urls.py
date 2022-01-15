d1cbcb0c98cc4819ccc5242587588d4e51c1c673
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-01-16 04:45:07 +0530

    Fixed index url

--- allocation/urls.py
+++ allocation/urls.py
@@ -3,7 +3,7 @@ from allocation import views
 
 app_name = 'allocation'
 urlpatterns = [
-    path("index", views.index.as_view(), name="index"),
+    path("", views.index.as_view(), name="index"),
     path("signup", views.signup.as_view(user_type="student"), name="student_signup"),
     path("login", views.login.as_view(user_type="student"), name="student_login"),
     path("allocate/<int:user_id>/<slug:room_number>", views.roomAllocate.as_view(), name="room_allocate"),
--- room_allocation/urls.py
+++ room_allocation/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('allocation/', include('allocation.urls', namespace='allocation')),
+    path('', include('allocation.urls', namespace='allocation')),
 ]
