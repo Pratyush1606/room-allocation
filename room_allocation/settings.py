ffe527b40d4c711dd6098d2f1a70c29b75f06046
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-01-16 04:39:31 +0530

    Added Allowed Hosts

--- room_allocation/settings.py
+++ room_allocation/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (True if os.getenv("DJANGO_DEBUG")=="True" else False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["allocate-room.herokuapp.com", '127.0.0.1']
 
 AUTH_USER_MODEL = 'allocation.User'
 
